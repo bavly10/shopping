ea51e5d3db15a22b55b88520144888cb9eb0c37f
bavly <bavly.naguib@yahoo.com>
2022-06-04 16:20:13 +0200

    app bar

--- lib/modules/mainScreen/screen/HomeScreen.dart
+++ lib/modules/mainScreen/screen/HomeScreen.dart
@@ -4,6 +4,7 @@ import 'package:shopping/Cubit/cubit.dart';
 import 'package:shopping/Cubit/states.dart';
 import 'package:shopping/shared/compononet/arrowBack.dart';
 import 'package:shopping/shared/localization/translate.dart';
+import 'package:simple_animations/simple_animations.dart';
 
 import '../../../shared/my_colors.dart';
 
@@ -17,43 +18,19 @@ class HomeScreen extends StatelessWidget {
         final cubit= ShopCubit.get(context);
         return Scaffold(
           backgroundColor: myGrey,
-          appBar:cubit.changeAppBar?myAppBar(context):myAppBarSearch(context),
+          body: Column(
+            children: [
+              cubit.changeAppBar?myAppBar(context):myAppBarSearch(context)
+            ],
+          ),
         );
       },
     );
   }
-  AppBar myAppBar(context){
-    return AppBar(
-      backgroundColor:myGrey,
-      elevation: 0.0,
-      title: Text(mytranslate(context, "home")),
-      leading: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(decoration: const BoxDecoration(
-          shape: BoxShape.circle,
-          color: Colors.grey
-        ),child: IconButton(icon: Icon(Icons.search_sharp,size: 25,color: myBlue,),
-            onPressed: (){
-          ShopCubit.get(context).changeSearchAppBar();
-            } ))
-      ),
-      actions: [
-        MyArrowBack(onPress: (){
-       Navigator.pop(context);
-        },)
-      ],
-    );
-  }
-
-  AppBar myAppBarSearch(context){
-    return AppBar(
-      iconTheme: IconThemeData(opacity: 0),
-      backgroundColor:myGrey,
-      elevation: 0.0,
-    flexibleSpace: Container(
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(45.0),color: Colors.grey),
+  Widget myAppBar(context){
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
       child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Card(
             color:Colors.grey,
@@ -65,26 +42,72 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
             elevation: 1,
-            child: Icon(Icons.search_sharp,size: 50,color: myBlue,),
+            child: IconButton(icon: Icon(Icons.search_sharp,size: 35,color: myBlue,),
+                onPressed: (){
+                  ShopCubit.get(context).changeSearchAppBar();
+                }),
           ),
-          const SizedBox(width: 5,),
-          SizedBox(
-            width: MediaQuery.of(context).size.width*0.50,
-            child: TextField(
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText:mytranslate(context, "search"),
-              ),
-            ),
-          ),
-          const Spacer(),
+           SizedBox(width: MediaQuery.of(context).size.width*0.25,),
+          Text(mytranslate(context, "home"),style: TextStyle(color: myBlack,fontWeight: FontWeight.bold,fontSize: 25),),
+          Spacer(),
           MyArrowBack(onPress: (){
-            ShopCubit.get(context).changeSearchAppBar();
+            Navigator.pop(context);
           },)
         ],
       ),
-    ),
-      centerTitle: false,
+    );
+  }
+
+  Widget myAppBarSearch(context){
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: PlayAnimation<double>(
+        tween: Tween(begin: 0.0, end: MediaQuery.of(context).size.width*0.75),
+        duration: const Duration(seconds: 1),
+        delay: const Duration(milliseconds: 20),
+        curve: Curves.easeOut,
+        builder: (context, child, value) {
+          return Row(
+            children: [
+              Container(
+                width: value,
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(35.0),color: Colors.grey),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Card(
+                      color:Colors.grey,
+                      shape: const StadiumBorder(
+                        side: BorderSide(
+                          style: BorderStyle.solid,
+                          color:Colors.grey,
+                          width: 6.0,
+                        ),
+                      ),
+                      elevation: 1,
+                      child: Icon(Icons.search_sharp,size: 50,color: myBlue,),
+                    ),
+                    const SizedBox(width: 5,),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width*0.50,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText:mytranslate(context, "search"),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Spacer(),
+              MyArrowBack(onPress: (){
+                ShopCubit.get(context).changeSearchAppBar();
+              },)
+            ],
+          );
+        },
+      ),
     );
   }
 }
