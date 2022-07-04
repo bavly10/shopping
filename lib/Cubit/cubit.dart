baed12dfe08762a0c4deb493cc3b3fcdc47ae637
siham <siham1554@gmail.com>
2022-07-04 15:48:45 +0300

    products+image

--- lib/Cubit/cubit.dart
+++ lib/Cubit/cubit.dart
@@ -3,16 +3,13 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shopping/Cubit/states.dart';
 import 'package:shopping/model/splash.dart';
 import 'package:shopping/modules/cart/cart.dart';
-import 'package:shopping/modules/login/login/login.dart';
 import 'package:shopping/modules/login/main.dart';
 import 'package:shopping/modules/mainScreen/screen/HomeScreen.dart';
-import 'package:shopping/shared/compononet/printFullText.dart';
 import 'package:shopping/shared/diohelper/dioHelpoer.dart';
 import 'package:shopping/shared/network.dart';
 import 'package:shopping/shared/shared_prefernces.dart';
-import 'package:http/http.dart' as http;
 
-import '../modules/mainScreen/screen/products.dart';
+import '../modules/mainScreen/screen/products/products.dart';
 
 class ShopCubit extends Cubit<ShopStates> {
   ShopCubit() : super(Shop_InitalState());
@@ -47,10 +44,12 @@ class ShopCubit extends Cubit<ShopStates> {
     emit(cons_Change_Language());
     return locale_cubit!;
   }
+
   Future<Locale> setLocale(String langCode) async {
     await CashHelper.putData("locale", langCode);
     return locale(langCode);
   }
+
   Future<Locale> getLocale() async {
     lang = CashHelper.getData("locale");
     return locale(lang!);
@@ -68,26 +67,27 @@ class ShopCubit extends Cubit<ShopStates> {
     currentindex = index;
     emit(ChangeIndexTabs());
   }
-  bool changeAppBar=true;
-  void changeSearchAppBar(){
-    changeAppBar=!changeAppBar;
+
+  bool changeAppBar = true;
+  void changeSearchAppBar() {
+    changeAppBar = !changeAppBar;
     emit(ChangeAppBar());
   }
 
-  int detailsIndex=0;
-  void getDetailsIndex(index){
-    detailsIndex=index;
+  int detailsIndex = 0;
+  void getDetailsIndex(index) {
+    detailsIndex = index;
     emit(ChangeIndexBar());
   }
 
-SplashModel? splashModel;
-  Future<void> getSplashData()async{
+  SplashModel? splashModel;
+  Future<void> getSplashData() async {
     emit(LoadingSplash());
     DioHelper.getData(url: splash).then((value) {
-      splashModel=SplashModel.fromJson(value.data);
+      splashModel = SplashModel.fromJson(value.data);
       print("done Splash model ${splashModel!.status}");
       emit(DoneSplash());
-    }).catchError((onError){
+    }).catchError((onError) {
       print(onError.toString());
       emit(ErrorSplash());
     });
---
+++ lib/modules/mainScreen/screen/products/widgets/products_card.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Stack(alignment: AlignmentDirectional.centerEnd, children: [
+        Align(
+          alignment: Alignment.topRight,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 40,
+              width: 40,
+              decoration: BoxDecoration(
+                  color: Colors.black,
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(10)),
+              child: Center(
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.favorite,
+                    color: Colors.white,
+                    size: 20,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        const Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Align(
+            alignment: Alignment.topLeft,
+            child: Image(
+              image: AssetImage("assets/x1.png"),
+              width: double.infinity,
+              height: 100,
+
+              // fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Positioned(
+          top: 100,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    'تيشرت اسود',
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                        height: 1.4, fontSize: 14, fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'يوجد الوان مختلفه',
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  style: TextStyle(
+                    height: 1.4,
+                    fontSize: 12,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Align(
+            alignment: AlignmentDirectional.bottomStart,
+            child: Text(
+              '80.00دينار',
+              overflow: TextOverflow.ellipsis,
+              // maxLines: 2,
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+            ),
+          ),
+        ),
+      ]),
+
+      /*   
+              
+              SizedBox(
+                width: 10,
+              ),*/
+    );
+  }
+}
