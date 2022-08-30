78341b00425e0fd12c2926e69abc465676e19db0
siham <siham1554@gmail.com>
2022-08-30 23:27:35 +0300

    chart

--- lib/modules/Customer/Static/static.dart
+++ lib/modules/Customer/Static/static.dart
@@ -6,6 +6,7 @@ import 'package:shopping/modules/Customer/cubit/cubit.dart';
 import 'package:shopping/modules/Customer/cubit/state.dart';
 import 'package:shopping/modules/test.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:shopping/shared/compononet/componotents.dart';
 import 'package:shopping/shared/my_colors.dart';
 
 class StaticMain extends StatelessWidget {
@@ -25,11 +26,12 @@ class StaticMain extends StatelessWidget {
     ];
     List<charts.Series<EarningsTimeline, String>> timeline1 = [
       charts.Series(
-          id: "Subscribers",
-          data: listEarning1,
-          domainFn: (EarningsTimeline timeline, _) => timeline.customer!,
-          measureFn: (EarningsTimeline timeline, _) => timeline.earning,
-          colorFn: (EarningsTimeline timeline, _) => timeline.barColor!),
+        id: "Subscribers",
+        data: listEarning1,
+        domainFn: (EarningsTimeline timeline, _) => timeline.customer!,
+        measureFn: (EarningsTimeline timeline, _) => timeline.earning,
+        colorFn: (EarningsTimeline timeline, _) => timeline.barColor!,
+      ),
     ];
 
     return BlocBuilder<CustomerCubit, CustomerStates>(
@@ -59,8 +61,13 @@ class StaticMain extends StatelessWidget {
           ));
         });
         return Scaffold(
-            appBar:
-                AppBar(title: Text("My Total Money:${cubit.data!.totalEarn} ")),
+            backgroundColor: myGrey,
+            appBar: AppBar(
+                backgroundColor: myGrey,
+                title: Text(
+                  "Statistics ",
+                  style: textStyle1,
+                )),
             body: Column(
               children: [
                 Padding(
@@ -69,8 +76,12 @@ class StaticMain extends StatelessWidget {
                     children: [
                       Text(
                         "My Total Money",
+                        style: textStyle,
                       ),
-                      Text(cubit.data!.totalEarn.toString())
+                      Text(
+                        cubit.data!.totalEarn.toString(),
+                        style: textStyle1,
+                      )
                     ],
                   ),
                 ),
@@ -80,7 +91,8 @@ class StaticMain extends StatelessWidget {
                     child: Column(
                       children: [
                         Padding(
-                          padding: const EdgeInsets.all(20.0),
+                          padding: const EdgeInsets.only(
+                              left: 30.0, right: 30, top: 8, bottom: 8),
                           child: TabBar(
                             indicator: BoxDecoration(
                                 color: myBlue,
@@ -88,15 +100,18 @@ class StaticMain extends StatelessWidget {
                             labelColor: myGrey,
                             unselectedLabelColor: Colors.black,
                             controller: controlle,
-                            tabs: const [
+                            tabs: [
                               Tab(
                                 child: Text(
                                   "Year",
+                                  style: textStyle,
                                 ),
                               ),
                               Tab(
-                                text: "Month",
-                              ),
+                                  child: Text(
+                                "Month",
+                                style: textStyle,
+                              )),
                             ],
                           ),
                         ),
@@ -128,21 +143,86 @@ class StaticMain extends StatelessWidget {
                                     child: Padding(
                                       padding: const EdgeInsets.all(8.0),
                                       child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.end,
                                         children: <Widget>[
-                                          Row(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.end,
-                                            children: [
-                                              Text(
-                                                "الارباح",
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Container(
+                                              color: myGrey,
+                                              child: Row(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.center,
+                                                children: [
+                                                  Text(
+                                                    "Earn: ",
+                                                    style: textStyle,
+                                                  ),
+                                                  Text(
+                                                    cubit.data!.totalEarn
+                                                        .toString(),
+                                                    style: textStyle,
+                                                  )
+                                                ],
                                               ),
-                                              Text(cubit.data!.totalEarn
-                                                  .toString())
-                                            ],
+                                            ),
                                           ),
                                           Expanded(
                                             child: charts.BarChart(
                                               timeline,
+                                              domainAxis: const charts
+                                                      .OrdinalAxisSpec(
+                                                  renderSpec: charts
+                                                      .SmallTickRendererSpec(
+
+                                                          // Tick and Label styling here.
+                                                          labelStyle: charts
+                                                              .TextStyleSpec(
+                                                                  fontSize:
+                                                                      14, // size in Pts.
+                                                                  color: charts
+                                                                      .MaterialPalette
+                                                                      .black),
+
+                                                          // Change the line colors to match text color.
+                                                          lineStyle: charts
+                                                              .LineStyleSpec(
+                                                                  thickness: 2,
+                                                                  color: charts
+                                                                      .MaterialPalette
+                                                                      .black))),
+                                              primaryMeasureAxis: charts
+                                                  .NumericAxisSpec(
+                                                      showAxisLine: true,
+                                                      renderSpec: charts
+                                                          .GridlineRendererSpec(
+                                                              axisLineStyle: charts
+                                                                  .LineStyleSpec(
+                                                                      thickness:
+                                                                          2,
+                                                                      color: charts
+                                                                          .MaterialPalette
+                                                                          .black),
+
+                                                              // Tick and Label styling here.
+                                                              labelStyle: charts
+                                                                  .TextStyleSpec(
+                                                                      lineHeight:
+                                                                          1.5,
+                                                                      fontWeight:
+                                                                          "bold",
+                                                                      fontSize:
+                                                                          16, // size in Pts.
+                                                                      color: charts
+                                                                          .MaterialPalette
+                                                                          .black),
+
+                                                              // Change the line colors to match text color.
+                                                              lineStyle: charts.LineStyleSpec(
+                                                                  thickness: 0,
+                                                                  color: charts
+                                                                      .MaterialPalette
+                                                                      .black))),
                                               animate: true,
                                             ),
                                           ),
@@ -175,21 +255,86 @@ class StaticMain extends StatelessWidget {
                                     child: Padding(
                                       padding: const EdgeInsets.all(8.0),
                                       child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.end,
                                         children: <Widget>[
-                                          Row(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.end,
-                                            children: [
-                                              Text(
-                                                "الارباح",
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Container(
+                                              color: myGrey,
+                                              child: Row(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.center,
+                                                children: [
+                                                  Text(
+                                                    "Earn: ",
+                                                    style: textStyle,
+                                                  ),
+                                                  Text(
+                                                    cubit.data!.totalEarn
+                                                        .toString(),
+                                                    style: textStyle,
+                                                  )
+                                                ],
                                               ),
-                                              Text(cubit.data!.totalEarn
-                                                  .toString())
-                                            ],
+                                            ),
                                           ),
                                           Expanded(
                                             child: charts.BarChart(
                                               timeline1,
+                                              domainAxis: const charts
+                                                      .OrdinalAxisSpec(
+                                                  renderSpec: charts
+                                                      .SmallTickRendererSpec(
+
+                                                          // Tick and Label styling here.
+                                                          labelStyle: charts
+                                                              .TextStyleSpec(
+                                                                  fontSize:
+                                                                      18, // size in Pts.
+                                                                  color: charts
+                                                                      .MaterialPalette
+                                                                      .black),
+
+                                                          // Change the line colors to match text color.
+                                                          lineStyle: charts
+                                                              .LineStyleSpec(
+                                                                  thickness: 2,
+                                                                  color: charts
+                                                                      .MaterialPalette
+                                                                      .black))),
+                                              primaryMeasureAxis: charts
+                                                  .NumericAxisSpec(
+                                                      showAxisLine: true,
+                                                      renderSpec: charts
+                                                          .GridlineRendererSpec(
+                                                              axisLineStyle: charts
+                                                                  .LineStyleSpec(
+                                                                      thickness:
+                                                                          2,
+                                                                      color: charts
+                                                                          .MaterialPalette
+                                                                          .black),
+
+                                                              // Tick and Label styling here.
+                                                              labelStyle: charts
+                                                                  .TextStyleSpec(
+                                                                      lineHeight:
+                                                                          1.5,
+                                                                      fontWeight:
+                                                                          "bold",
+                                                                      fontSize:
+                                                                          18, // size in Pts.
+                                                                      color: charts
+                                                                          .MaterialPalette
+                                                                          .black),
+
+                                                              // Change the line colors to match text color.
+                                                              lineStyle: charts.LineStyleSpec(
+                                                                  thickness: 0,
+                                                                  color: charts
+                                                                      .MaterialPalette
+                                                                      .black))),
                                               animate: true,
                                             ),
                                           ),
--- lib/shared/compononet/componotents.dart
+++ lib/shared/compononet/componotents.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:shopping/shared/my_colors.dart';
 
- navigateTo(context, widget) => Navigator.push(context, MaterialPageRoute(builder: (context) => widget));
+navigateTo(context, widget) =>
+    Navigator.push(context, MaterialPageRoute(builder: (context) => widget));
 
-void navigateToFinish(context, widget) => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => widget));
+void navigateToFinish(context, widget) => Navigator.pushReplacement(
+    context, MaterialPageRoute(builder: (context) => widget));
+var textStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 18,
+);
+var textStyle1 =
+    TextStyle(fontWeight: FontWeight.bold, fontSize: 25, color: myBlue);
